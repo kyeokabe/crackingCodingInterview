0842e184aecf2440c241411664b48fb3ccc29429
Kye Okabe <kyeokabe@gmail.com>
2019-03-01 16:18:31 -0800

    Update array_element_sum.cpp

--- code/array_element_sum.cpp
+++ code/array_element_sum.cpp
@@ -1,23 +1,28 @@
-//incorrect... needs work
+//write a recursion function that calculates the sum of int elements ... CS32 Lec 8 problem
 
 #include <iostream>
 #include<stdio.h>
 
-int elementSum(int N[]){
-    
-    int no_of_elements=sizeof(N)/sizeof(N[0]);
-    if (no_of_elements==1)
-    return N[0];
-    
-    return (N[no_of_elements-1]+elementSum(N[no_of_elements-2]));
-}
+int elementSum(int* N, int size);
 
 int main() {
     
-    int foo[5]={2,9,7,4,0};
+    int foo[5]={2,9,7,4,1};
+    int size_array = sizeof(foo)/sizeof(foo[0]);
     
-    int fooSum=elementSum(foo);
+    printf ("Size of array is %d\n", size_array);
     
+    int fooSum=elementSum(foo, size_array);
+    printf ("\n");
     printf ("Sum of array is %d\n", fooSum);
     return 0;
 }
+
+int elementSum(int* N, int size_array){
+    
+    printf ("%d\n", N[0]);
+    if (size_array==1)
+        return N[0];
+    return (N[0] + elementSum(N+1, size_array-1));
+}
+
