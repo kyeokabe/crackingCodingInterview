064ec24c74e33d8b538801eb705d7852ea901330
Kye Okabe <kyeokabe@gmail.com>
2019-03-14 12:20:52 -0700

    Create 5.6_Conversion.cpp

---
+++ code/ch5/5.6_Conversion.cpp
@@ -0,0 +1,67 @@
+//5.6 ... Conversion
+//write a function to determine the number of bits you would need to flip to convert int A to int B
+//eg. input: 29 (11101), 15 (01111) -> output: 2
+
+/*
+all about knowing the & operator
+*/
+
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+int binary_digits(int a);
+int overlap_digits(int a);
+
+int main() {
+    int a=15;
+    int b=29;
+    
+    int a_size,b_size,maxdigit;
+    
+    a_size=binary_digits(a);
+    b_size=binary_digits(b);
+    int overlapdigit=overlap_digits((a&b));
+
+    if (a_size>=b_size)
+        maxdigit=a_size;
+    else
+        maxdigit=b_size;
+
+    cout<<"input 1 = "<<a<<endl;
+    cout<<"input 2 = "<<b<<endl<<endl;
+    cout<<"max binary digit = "<<maxdigit<<endl;
+    cout<<"overlap binary digits in decimal= "<<(a&b)<<endl;
+    cout<<"overlaping binary digits = "<<overlapdigit<<endl;
+
+    cout<<endl;
+    cout<<"bits neededed to flip = "<<maxdigit-overlapdigit<<endl<<endl;
+    
+    return 0;
+}
+
+int binary_digits(int a){
+    if (a<=0)
+        return 0;
+    
+    int count=0;
+    
+    while(a!=0){
+        a=a/2;
+        count++;
+    }
+    return count;
+}
+
+int overlap_digits(int a){
+    if (a<=0)
+        return 0;
+    int count=0;
+    while(a!=0){
+        if (a%2==1){
+            count++;
+        }
+        a=a/2;
+    }
+    return count;
+}
