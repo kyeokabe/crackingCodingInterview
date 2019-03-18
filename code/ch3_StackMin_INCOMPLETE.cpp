d8cf205d4885b5539ff315ca2fd6b4e4c38fc993
Kye Okabe <kyeokabe@gmail.com>
2019-03-18 15:38:13 -0700

    Create ch3_StackMin_INCOMPLETE.cpp

---
+++ code/ch3_StackMin_INCOMPLETE.cpp
@@ -0,0 +1,55 @@
+// 3.2 Stack Min
+
+/*
+Should know how to make a class that uses its class.
+*/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+const int SIZE=100;
+
+bool isUnique(string a);
+
+//class Stack1;
+
+class Stack{
+public:
+    Stack(){
+        for (int i=0;i<SIZE;i++)
+            m_array[i]=0;
+        current_index=0;
+        m_min_initialized=false;
+    }
+    
+    void push(int a){
+        if (!m_min_initialized)
+            m_min=a;
+        if (m_min>a)
+            m_min=a;
+        m_array[current_index]=a;
+        current_index++;
+    }
+    
+    int pop(){
+        current_index--;
+        int temp=m_array[current_index];
+        m_array[current_index]=0;
+        return temp;
+    }
+    
+private:
+    int m_array[SIZE];
+    int current_index;
+    int m_min;
+    bool m_min_initialized;
+    Stack min;
+};
+
+int main() {
+    
+    Stack foo1;
+    
+    return 0;
+}
