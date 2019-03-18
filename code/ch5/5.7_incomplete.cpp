475ee2b5b0ae2b4a5616341649654e32bff34c2c
Kye Okabe <kyeokabe@gmail.com>
2019-03-18 12:44:41 -0700

    Update 5.7_incomplete.cpp

--- code/ch5/5.7_incomplete.cpp
+++ code/ch5/5.7_incomplete.cpp
@@ -2,49 +2,54 @@
 //write a function to swap even and odd bits with minimum instructions as possible.
 
 /*
-all about knowing the & operator
+Made several small errors. More of a grit problem.
+Exponentials can be obtained by
+ 
+#include <math.h>
+ int foo = pow(2,3);
 */
 
 #include <iostream>
 #include<stdio.h>
+#include <math.h>
 using namespace std;
 
-int bitswapEvenOdd(int a, int b[]);
+void bitswapEvenOdd(int a, int b[]);
 
-int binary_digits(int a);
+int get_num_bin_digits(int a);
 int arrayToBinary(int b[], int sizeofb);
 
 /*
-0101
-1010
+ 0101
+ 1010
  */
 int main() {
-    int a=3;
-    int a_size;
-    a_size=binary_digits(a);
-    int b[a_size+1];
+    int a=5;
+    int a_num_digits;
+    a_num_digits=get_num_bin_digits(a);
+    int b[a_num_digits+1];
     //technically don't need to initialize
-    for (int i=0;i<a_size+1;i++)
+    for (int i=0;i<a_num_digits+1;i++)
         b[i]=0;
-    int sizeOfb = sizeof(b)/sizeof(b[0]);
-
-    cout<<"size of b = "<<sizeOfb<<endl;
+    cout<<"input = "<<a<<endl;
+    cout<<"bin digits needed = "<<a_num_digits+1<<endl;
     
     bitswapEvenOdd(a,b);
     
-    cout<<"b[0] = "<<b[0]<<endl;
+    cout<<"reversed binary representation: ";
+    for (int i=0;i<a_num_digits+1;i++)
+        cout<<b[i];
+    cout<<endl;
     
-    cout<<"answer is: "<<arrayToBinary(b, sizeOfb)<<endl;
-    return arrayToBinary(b, sizeOfb);
-
+    cout<<"answer is: "<<arrayToBinary(b, a_num_digits+1)<<endl<<endl;
+    return 0;
+    
 }
 
-int binary_digits(int a){
+int get_num_bin_digits(int a){
     if (a<=0)
         return 0;
-    
     int count=0;
-    
     while(a!=0){
         a=a/2;
         count++;
@@ -53,43 +58,47 @@ int binary_digits(int a){
 }
 
 
-int bitswapEvenOdd(int a,int b[]){
+void bitswapEvenOdd(int a,int b[]){
     int i=0;
     if (a==0)
-        return 0;
+        return;
+
     while (a!=0){
-        if ((a%2==1) && (a%4==1)){
-            b[i]=0;
-            b[i+1]=0;
-        }
-        if ((a%2==1) && (a%4==0)){
-            b[i]=0;
-            b[i+1]=1;
-        }
-        if ((a%2==0) && (a%4==1)){
-            b[i]=1;
-            b[i+1]=0;
-        }
-        if ((a%2==0) && (a%4==0)){
+        if ((a%2==1) && ((a/2)%2==1)){
             b[i]=1;
             b[i+1]=1;
         }
-        a=a/4;
+        if ((a%2==1) && ((a/2)%2==0)){
+            b[i]=0;
+            b[i+1]=1;
+            cout<<"i = "<<i<<", a = "<<a<<endl;
+
+        }
+        if ((a%2==0) && ((a/2)%2==1)){
+            b[i]=1;
+            b[i+1]=0;
+
+        }
+        if ((a%2==0) && ((a/2)%2==0)){
+            b[i]=0;
+            b[i+1]=0;
+        }
+        a=a/2;
+        a=a/2;
         i=i+2;
     }
-    return 0;
 }
 
 int arrayToBinary(int b[], int sizeOfb){
     int foo=0;
-    int count=0;
+    int count=1;
     
     if (b[0]==1)
         foo=1;
     
     for (int i=1;i<sizeOfb;i++){
         if (b[i]==1)
-            foo=foo+2*count;
+            foo=foo+pow(2,count);
         count++;
     }
     return foo;
