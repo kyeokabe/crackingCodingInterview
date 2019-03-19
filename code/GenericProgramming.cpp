a10748be0c5b85801c4b8b1d39e8ae2f3bfb8230
Kye Okabe <kyeokabe@gmail.com>
2019-03-19 14:53:42 -0700

    Update and rename operatorDefinition.cpp to GenericProgramming.cpp

--- code/GenericProgramming.cpp
+++ code/GenericProgramming.cpp
@@ -3,9 +3,15 @@
 
 //operator> and const are tricky parts
 
+//1) Generic Comparisons
+
 bool operator>(const Dog &a, const Dog &b){
   if (a.weight > b.weight)
     return true;
   else
     return false;
 }
+
+//2) Generic Functions
+
+//3) Generic Classes
