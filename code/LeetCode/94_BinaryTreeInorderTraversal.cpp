b3442631775f1ab88e352ef20bca8381caf9835c
Kye Okabe <kyeokabe@gmail.com>
2019-03-24 14:46:02 -0700

    Create 94_BinaryTreeInorderTraversal.cpp

---
+++ code/LeetCode/94_BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> foo;
+        if (root==nullptr)
+            return foo;
+        inorderTraversal(root,foo);
+        return foo;
+    }
+    
+    void inorderTraversal(TreeNode* root, vector<int> &foo){
+        if (root==nullptr)
+            return;
+        inorderTraversal(root->left,foo);
+        foo.push_back(root->val);
+        inorderTraversal(root->right,foo);
+    }
+};
