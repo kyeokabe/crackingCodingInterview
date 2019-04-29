095feb34d26d5aeded258d6836730775d494534d
Kye Okabe <kyeokabe@gmail.com>
2019-04-28 19:56:54 -0700

    Create 342_PowerOfFour.cpp

---
+++ code/LeetCode/342_PowerOfFour.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if(num<=0)
+            return false;
+        while(num%4==0)
+            num=num/4;
+        if(num!=1)
+            return false;
+        return true;
+    }
+};
