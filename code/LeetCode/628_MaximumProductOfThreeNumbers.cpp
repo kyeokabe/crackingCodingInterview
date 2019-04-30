47ec0aae5f8e470a6f18d6c8aefe3ef9f22a7474
Kye Okabe <kyeokabe@gmail.com>
2019-04-30 11:59:47 -0700

    Create 628_MaximumProductOfThreeNumbers.cpp

---
+++ code/LeetCode/628_MaximumProductOfThreeNumbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        int a=nums[0]*nums[1]*nums[nums.size()-1];
+        int b=nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3];
+        if(a>b)
+            return a;
+        return b;
+    }
+};
