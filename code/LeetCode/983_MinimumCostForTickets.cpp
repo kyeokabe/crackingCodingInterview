85b801511e09677e3f91f2091615e45747ff1c90
Kye Okabe <kyeokabe@gmail.com>
2019-09-18 14:18:46 -0700

    Create 983_MinimumCostForTickets.cpp

---
+++ code/LeetCode/983_MinimumCostForTickets.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        if(days.size()==0)
+            return 0;
+        int i=0;
+        //v[i] stores the minimum cost for days[i]
+        vector<int> v(days.size(),INT_MIN);
+        return mincost(i,days,costs,v);
+    }
+    
+    int mincost(int i, vector<int>& days, vector<int>& costs, vector<int> &v){
+        if(i>=days.size())
+            return 0;
+        
+        if(v[i]!=INT_MIN)
+            return v[i];
+        
+        else{
+        
+            //calculate cost of buying 1 day pass
+            //starting on day i
+            int cost1=costs[0]+mincost(i+1,days,costs,v);
+       
+            //calculate cost of buying 7 day pass
+            //starting on day i
+            int day7=days[i]+7-1;
+            int i7=i;
+            for(int j=i;j<days.size();j++){
+                if(days[j]<=day7)
+                    i7++;
+            }
+            int cost7=costs[1]+mincost(i7,days,costs,v);
+        
+            //calculate cost of buying 30 day pass
+            //starting on day i
+            int day30=days[i]+30-1;
+            int i30=i;
+            for(int j=i;j<days.size();j++){
+                if(days[j]<=day30)
+                    i30++;
+            }
+            int cost30=costs[2]+mincost(i30,days,costs,v);
+            
+            //store minimum cost for day i in vector v
+            v[i]=min(min(cost1,cost7),cost30);
+            return v[i];
+        }
+    }
+    
+};
