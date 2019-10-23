6ee7f66cde07d09cf8fc4cff0c5956515f5ed6c6
Kye Okabe <kyeokabe@gmail.com>
2019-10-22 18:10:42 -0700

    Update 494_TargetSum.cpp

--- code/LeetCode/494_TargetSum.cpp
+++ code/LeetCode/494_TargetSum.cpp
@@ -1,3 +1,34 @@
+//adapred from
+//https://leetcode.com/problems/target-sum/discuss/97335/Short-Java-DP-Solution-with-Explanation
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+            sum+=nums[i];
+        if(sum<S||-sum>S)
+            return 0;
+        
+        vector<int> v1(nums.size(),0);
+        vector<vector<int>> v(2*sum+1,v1);
+        
+        v[+nums[0]+sum][0]++;
+        v[-nums[0]+sum][0]++;
+        
+        for(int i=1;i<nums.size();i++){
+            for(int j=0;j<2*sum+1;j++){
+                if(j+nums[i]<2*sum+1)
+                    v[j+nums[i]][i]+=v[j][i-1];
+                if(j-nums[i]>=0)
+                    v[j-nums[i]][i]+=v[j][i-1];
+        
+            }
+        }
+        return int(v[S+sum][nums.size()-1]);
+    }
+};
+
+/*
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
@@ -22,3 +53,4 @@ public:
         }
     }
 };
+*/
